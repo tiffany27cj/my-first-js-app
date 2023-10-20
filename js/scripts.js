30e501b3c3591a04bff4a724b061e71aabec502f
tiffany27cj <59047830+tiffany27cj@users.noreply.github.com>
2023-10-20 12:22:49 +0200

    Update scripts.js
    Add five pokemon objects and an empty pokemonList array

---
+++ js/scripts.js
@@ -0,0 +1,26 @@
+let pokemonList = []
+let pokemon1 = {
+    name: "Golduck",
+    height: 1.7,
+    type: ['water']
+};
+let pokemon2 = {
+    name: "Gloom",
+    height: 0.8,
+    types: ['grass', 'poison']
+};
+let pokemon3 = {
+    name: "Beedrill",
+    height: 1,
+    types: ['bug']
+};
+let pokemon4 = {
+    name: "Spearow",
+    height: 0.3,
+    types: ['flying']
+};
+let pokemon5 = {
+    name: 0.8,
+    types: ['poison']
+};
+
