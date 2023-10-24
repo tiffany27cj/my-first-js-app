f9972b35fe3d679313d4def68b8529422d3d77db
tiffany27cj <59047830+tiffany27cj@users.noreply.github.com>
2023-10-24 13:20:57 +0200

    Update scripts.js
    Fix variable initialization, and the definition of height

--- js/scripts.js
+++ js/scripts.js
@@ -1,35 +1,35 @@
-let pokemonList = [{ pokemon1 }, { pokemon2 }, { pokemon3 }, { pokemon4 }, { pokemon5 }]
-let pokemon1 = {
-    name: "Golduck",
-    height: 1.7,
-    type: ['water']
-};
-let pokemon2 = {
-    name: "Gloom",
-    height: 0.8,
-    types: ['grass', 'poison']
-};
-let pokemon3 = {
-    name: "Beedrill",
-    height: 1,
-    types: ['bug']
-};
-let pokemon4 = {
-    name: "Spearow",
-    height: 0.3,
-    types: ['flying']
-};
-let pokemon5 = {
-    name: "Caterpie",
-    height: 1,
-    types: ['bug']
-};
-
-for (let i = 0; i <= pokemonList.length; i++) {
-    if (height >= 1) {
-        document.write(pokemonList.name[i] + " \(" + "height: " + pokemonList.height[i] + "\)" + " -" + "Wow, that\'s big!");
+let pokemonList = [
+    {
+        name: "Golduck",
+        height: 1.7,
+        types: ['water']
+    },
+    {
+        name: "Gloom",
+        height: 0.8,
+        types: ['grass', 'poison']
+    },
+    {
+        name: "Beedrill",
+        height: 1,
+        types: ['bug']
+    },
+    {
+        name: "Spearow",
+        height: 0.3,
+        types: ['flying']
+    },
+    {
+        name: "Caterpie",
+        height: 1,
+        types: ['bug']
     }
-    else {
-        document.write(pokemonList.name[i] + " \(" + "height: " + pokemonList.height[i] + "\)");
+];
+
+for (let i = 0; i < pokemonList.length; i++) {
+    if (pokemonList[i].height >= 1) {
+        document.write(pokemonList[i].name + " (" + "height: " + pokemonList[i].height + ") - " + "Wow, that's big!<br>");
+    } else {
+        document.write(pokemonList[i].name + " (" + "height: " + pokemonList[i].height + ")<br>");
     }
 }
