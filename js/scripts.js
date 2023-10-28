1a6ed311b826b03d584df79e5105d6c6d974c852
tiffany27cj <59047830+tiffany27cj@users.noreply.github.com>
2023-10-28 09:51:25 +0200

    Update scripts.js
    Wrap pokemonList array in an IIFE

--- js/scripts.js
+++ js/scripts.js
@@ -1,35 +1,49 @@
-let pokemonList = [
-    {
-        name: "Golduck",
-        height: 1.7,
-        types: ['water']
-    },
-    {
-        name: "Gloom",
-        height: 0.8,
-        types: ['grass', 'poison']
-    },
-    {
-        name: "Beedrill",
-        height: 1,
-        types: ['bug']
-    },
-    {
-        name: "Spearow",
-        height: 0.3,
-        types: ['flying']
-    },
-    {
-        name: "Caterpie",
-        height: 1,
-        types: ['bug']
-    }
-];
 
-for (let i = 0; i < pokemonList.length; i++) {
-    if (pokemonList[i].height >= 1) {
-        document.write(pokemonList[i].name + " (" + "height: " + pokemonList[i].height + ") - " + "Wow, that's big!<br>");
-    } else {
-        document.write(pokemonList[i].name + " (" + "height: " + pokemonList[i].height + ")<br>");
-    }
+let pokemonRepository = (function () {
+    let pokemonList = [
+        {
+            name: "Golduck",
+            height: 1.7,
+            types: ['water']
+        },
+        {
+            name: "Gloom",
+            height: 0.8,
+            types: ['grass', 'poison']
+        },
+        {
+            name: "Beedrill",
+            height: 1,
+            types: ['bug']
+        },
+        {
+            name: "Spearow",
+            height: 0.3,
+            types: ['flying']
+        },
+        {
+            name: "Caterpie",
+            height: 1,
+            types: ['bug']
+        }
+    ];
+})();
+
+function add(pokemon) {
+    pokemonList.push(pokemon);
 }
+
+function getAll() {
+    return pokemonList;
+}
+
+return {
+    add: add,
+    getAll: getAll
+}();
+
+console.log(pokemonRepository.getAll());
+pokemonRepository.add({ name: 'Pikachu' });
+console.log(pokemonRepository.getAll());
+
+
