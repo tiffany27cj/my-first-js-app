92a0095d1bbac2406bee4995664a784080b36979
tiffany27cj <59047830+tiffany27cj@users.noreply.github.com>
2023-10-31 15:06:21 +0100

    Update scripts.js
    Use forEach loop inside IIFE using getAll()

--- js/scripts.js
+++ js/scripts.js
@@ -1,51 +1,51 @@
-// let pokemonRepository = (function () {
-let pokemonList = [
-    {
-        name: "Golduck",
-        height: 1.7,
-        types: ['water']
-    },
-    {
-        name: "Gloom",
-        height: 0.8,
-        types: ['grass', 'poison']
-    },
-    {
-        name: "Beedrill",
-        height: 1,
-        types: ['bug']
-    },
-    {
-        name: "Spearow",
-        height: 0.3,
-        types: ['flying']
-    },
-    {
-        name: "Caterpie",
-        height: 1,
-        types: ['bug']
+let pokemonRepository = (function () {
+    let pokemonList = [
+        {
+            name: "Golduck",
+            height: 1.7,
+            types: ['water']
+        },
+        {
+            name: "Gloom",
+            height: 0.8,
+            types: ['grass', 'poison']
+        },
+        {
+            name: "Beedrill",
+            height: 1,
+            types: ['bug']
+        },
+        {
+            name: "Spearow",
+            height: 0.3,
+            types: ['flying']
+        },
+        {
+            name: "Caterpie",
+            height: 1,
+            types: ['bug']
+        }
+    ];
+
+    function add(pokemon) {
+        pokemonList.push(pokemon);
     }
-];
 
-function add(pokemon) {
-    pokemonList.push(pokemon);
-}
+    function getAll() {
+        return pokemonList;
+    }
 
-function getAll() {
-    return pokemonList;
-}
-
-// return {
-//   add: add,
-//  getAll: getAll
-// }
-// }) ();
+    return {
+        add: add,
+        getAll: getAll
+    }
+})();
 
 // console.log(pokemonRepository.getAll());
 // pokemonRepository.add({ name: 'Pikachu', height: 1, types: ['electric'] });
 // console.log(pokemonRepository.getAll());
 
 
-pokemonList.forEach(function (element) {
+pokemonRepository.getAll().forEach(function (element) {
     console.log(element);
 })
