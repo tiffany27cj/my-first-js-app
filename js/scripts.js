7dbe418dca2ce68d92558a55739ec9fccb7c6424
tiffany27cj <59047830+tiffany27cj@users.noreply.github.com>
2023-11-04 20:04:56 +0100

    Update scripts.js
    Add missing curly brace

--- js/scripts.js
+++ js/scripts.js
@@ -31,39 +31,40 @@ let pokemonRepository = (function () {
         if (typeof pokemon === 'object') {
             pokemonList.push(pokemon);
         }
+    }
 
-        function getAll() {
-            return pokemonList;
-        }
+    function getAll() {
+        return pokemonList;
+    }
 
-        function addListItem(pokemon) {
-            let pokemonList = document.querySelector(".pokemon-list");
-            let listPokemon = document.createElement("li");
-            let button = document.createElement("button");
-            button.innerText = pokemon.name;
-            button.classList.add("button-class");
-            listPokemon.appendChild(button);
-            pokemonList.appendChild(listPokemon);
-            button.addEventListener('click', function () {
-                showDetails(pokemon);
-            });
-        }
+    function addListItem(pokemon) {
+        let pokemonList = document.querySelector(".pokemon-list");
+        let listPokemon = document.createElement("li");
+        let button = document.createElement("button");
+        button.innerText = pokemon.name;
+        button.classList.add("button-class");
+        listPokemon.appendChild(button);
+        pokemonList.appendChild(listPokemon);
+        button.addEventListener('click', function () {
+            showDetails(pokemon);
+        });
+    }
 
-        function showDetails(pokemon) {
-            console.log(pokemon);
-        }
+    function showDetails(pokemon) {
+        console.log(pokemon);
+    }
 
-        return {
-            add: add,
-            getAll: getAll,
-            addListItem: addListItem,
-            showDetails: showDetails
-        }
-    }) ();
+    return {
+        add: add,
+        getAll: getAll,
+        addListItem: addListItem,
+        showDetails: showDetails
+    }
+})();
 
 
-    pokemonRepository.getAll().forEach(function (pokemon) {
-        pokemonRepository.addListItem(pokemon);
-    });
+pokemonRepository.getAll().forEach(function (pokemon) {
+    pokemonRepository.addListItem(pokemon);
+});
 
 
